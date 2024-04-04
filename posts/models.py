6ec1a7c01099696b3f23156f0e0ace0a97571982
unknown <jaym991776@gmail.com>
2024-04-04 19:41:05 -0400

    Edit media url

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from profiles.models import Profile
 
 # Create your models here.
 
-#class Profile(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-   # bio - models.TextField(blank=True)
-   # avatar = models.ImageField(default='avatar.png', upload_to='avatars')
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)    
+
+
+    def __str__(self):
+        return str(self.title)
