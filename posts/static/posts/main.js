798b155426ad9ec28b1fe6c05f87d9a84f2caca0
unknown <jaym991776@gmail.com>
2024-04-06 19:46:39 -0400

    Adding Limitation to the dropzon

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -101,7 +101,7 @@ const getData = () => {
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
-                                             <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})` : `Like (${el.count})`}</button>
+                                             <button class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})` : `Like (${el.count})`}</button>
                                         </form>
                                     </div>
     
@@ -138,6 +138,8 @@ loadBtn.addEventListener('click', () => {
 
 })
 
+let newPostId = null
+
 postForm.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -151,6 +153,7 @@ postForm.addEventListener('submit', e => {
         },
         success: function(response){
             console.log(response)
+            newPostId = response.id
             postsBox.insertAdjacentHTML('afterbegin', `
 
                 <div class="card mb-2">
@@ -161,11 +164,11 @@ postForm.addEventListener('submit', e => {
                     <div class="card-footer">
                         <div class="row">
                             <div class="col-2">
-                                <a href="#" class="btn btn-primary">Details</a>
+                                <a href="${url}${response.id}" class="btn btn-primary">Details</a>
                             </div>
                             <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${response.id}">
-                                    <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0) </button>
+                                    <button  class="btn btn-primary" id="like-unlike-${response.id}">Like (0) </button>
                                 </form>
                             </div>
 
@@ -202,6 +205,21 @@ closeBtns.forEach(btn => btn.addEventListener('click', () => {
         dropzone.classList.add('not-visible');
     }
 }))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone("#my-dropzone", {
+    url: 'upload/',
+    init: function() {
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId);
+        })
+    },
+    maxFiles: 5,
+    maxFilesize: 4,
+    acceptedFiles: '.png, .jpg, .jpeg'
+})
+
 
 
 getData()
